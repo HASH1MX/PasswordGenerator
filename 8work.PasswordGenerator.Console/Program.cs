e24ef6a28ae98edbd705b97f6e5b701cb9d8dab3
HASH1MX <hashim8work@gmail.com>
2023-12-25 20:27:14 +0100

    Set up

---
+++ 8work.PasswordGenerator.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+using _8work.PasswordGenerator.Library;
+
+namespace PasswordGeneratorTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PasswordGenerator PG = new PasswordGenerator();
+
+            string resultGenerate = PG.Generate();
+            Console.WriteLine(resultGenerate);
+        }
+    }
+}
---
+++ 8work.PasswordGenerator.Library/PasswordGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace _8work.PasswordGenerator.Library;
+
+public class PasswordGenerator
+{
+    public string Generate()
+    {
+        return "dg9V39XXph";
+    }
+}
